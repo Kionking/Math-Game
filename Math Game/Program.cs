23153115e1979a918f2ff2a1ff104e816077ae5a
kionk <kionk@LAPTOP-7BR4KFL8>
2023-09-29 15:43:28 +0200

    Add project files.

---
+++ Math Game/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Välkommen till mitt matemati spel!\n");
+
+Random random = new Random();
+string användarVal = "";
+
+// while loop
+while (användarVal != "6")
+{
+    // Meny med alternativ
+    Console.WriteLine("1. Addition");
+    Console.WriteLine("2. Subtraktion");
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("");
+    // Ta emot användarens val
+
+    // switch
+    // case som anropar mina metoder
+}
+
+
+// Metod för Addition
+// Metod för Subration
+// Metod för Division
+// Metod för Multiplikation
+// Metod för att se mina tidigare omgångar
